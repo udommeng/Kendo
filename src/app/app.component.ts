9153f33fb8df57c6ec77cfe631d9243b52adf2f8
Meng-MSI <udommeng@gmail.com>
2019-01-27 01:39:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { products } from './products';
 import { Component } from '@angular/core';
 
+import '@progress/kendo-angular-intl/locales/th/all';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstKendo';
+
+  public gridData: any[] = products;
+
+  public value: Date = new Date(2000, 2, 10);
+
+  public pieData: any = [
+    { category: 'ไทย', value: 20 },
+    { category: 'อังกฤษ', value: 100 },
+    { category: 'เลข', value: 30 }
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule, SparklineModule, StockChartModule } from '@progress/kendo-angular-charts';
 import { AppComponent } from './app.component';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { LOCALE_ID } from '@angular/core';
+import { GridModule } from '@progress/kendo-angular-grid';
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ChartsModule, SparklineModule, StockChartModule,
+    DateInputsModule, GridModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: LOCALE_ID, useValue: 'th-TH'}
+  ],
 })
-export class AppModule { }
+export class AppModule {
+}
+
